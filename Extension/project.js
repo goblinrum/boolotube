152d46ed3fb2a819a5f6ad1c8a9229c66d6fb0bd
Maanuj Vora <maanujvora@gmail.com>
2023-10-28 16:30:02 -0700

    Update project.js

--- Extension/project.js
+++ Extension/project.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var project = document.getElementById('project').value;
     chrome.runtime.sendMessage({project: project, action: 'openUrl'}, function(response) {
       chrome.windows.create({
-          url: response.data.my_reaction_url,
+          url: response.data.original_video_url,
           type: 'popup', width: 400, height: 400,
       });
     });
