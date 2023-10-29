bb5023e582e20be4cb91d64a9b99a85e14639c94
Bill Hu <hubi@berkeley.edu>
2023-10-28 17:18:54 -0700

    remove template stuff

--- BooloTube/state.py
+++ BooloTube/state.py
@@ -1,21 +1,8 @@
 """Base state for the app."""
 
 import reflex as rx
-import asyncio
 
 class State(rx.State):
-
-    # The current question being asked.
-    question: str
-
-    # Keep track of the chat history as a list of (question, answer) tuples.
-    chat_history: list[tuple[str, str]]
-
-    def answer(self):
-        # Our chatbot is not very smart right now...
-        answer = "I don't know!"
-        self.chat_history.append((self.question, answer))
-    project_id: str
 
     @rx.var
     def project_id(self) -> str:
@@ -25,23 +12,3 @@ class State(rx.State):
     @rx.var
     def secret_id(self) -> str:
         return self.get_query_params().get('pid', 'no pid')
-
-async def answer(self):
-    # Our chatbot is not very smart right now...
-    answer = "I don't know!"
-    self.chat_history.append((self.question, ""))
-
-    # Clear the question input.
-    self.question = ""
-    # Yield here to clear the frontend input before continuing.
-    yield
-
-    for i in range(len(answer)):
-        # Pause to show the streaming effect.
-        await asyncio.sleep(0.1)
-        # Add one letter at a time to the output.
-        self.chat_history[-1] = (
-            self.chat_history[-1][0],
-            answer[: i + 1],
-        )
-        yield
